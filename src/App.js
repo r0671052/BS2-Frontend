0f79f0e9ba18080886767424f9165fc47fb0c2ec
Thomas <thomas.pede@student.ucll.be>
2022-12-09 20:46:43 +0100

    api url fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CollectionForm from './Form'
 
 class App extends Component{
 
-    API_URL="http://localhost:8080"
+    API_URL="http://109.89.134.55:8081"
 
     state = {
       products: [],
